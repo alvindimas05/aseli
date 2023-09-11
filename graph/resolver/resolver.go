4e2e2fe039c024ca05daa8a354ded353efa1398f
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2023-09-11 19:17:28 +0700

    feat: split resolver

--- graph/resolver/resolver.go
+++ graph/resolver/resolver.go
@@ -1,7 +1,18 @@
 package resolver
 
+import "aseli-api/graph/generated"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct{}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
