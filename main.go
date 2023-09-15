47694a0d6fcbdaa63fe20826bb02ec6e1b4737fe
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2023-09-15 13:49:03 +0700

    feat: static post images

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"aseli-api/graph/generated"
+	"aseli-api/graph/helper"
 	"aseli-api/graph/middleware"
 	"aseli-api/graph/resolver"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
+	// "github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
@@ -30,7 +33,7 @@ func main() {
 }
 
 func SetServer() {
-	port := os.Getenv("SERVER_PORT")
+	host, port := os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT")
 	router := chi.NewRouter()
 
 	router.Use(middleware.UserMiddleware)
@@ -38,9 +41,40 @@ func SetServer() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
 	// srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
 
-	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// router.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe("127.0.0.1:"+port, router))
+	fs := CustomFileServer(http.Dir(helper.MkdirImages()))
+	router.Handle("/images/*", http.StripPrefix("/images/", fs))
+	// router.Handle("/images/*", http.StripPrefix("/images/", http.FileServer(http.Dir(helper.MkdirImages()))))
+
+	// log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	addr := fmt.Sprintf("%s:%s", host, port)
+	log.Printf("Server is running on http://%s", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
+}
+
+func CustomFileServer(root http.Dir) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		filePath := filepath.Clean(r.URL.Path)
+		file, err := root.Open(filePath)
+
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		defer file.Close()
+
+		fileInfo, err := file.Stat()
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		if fileInfo.IsDir() {
+			http.NotFound(w, r)
+			return
+		}
+		http.FileServer(root).ServeHTTP(w, r)
+	})
 }
