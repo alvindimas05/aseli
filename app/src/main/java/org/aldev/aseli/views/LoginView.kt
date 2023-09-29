bf3bd2845b5735d4365db8112b0baf1fda67f4cb
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-09-29 22:17:18 +0700

    feat: check user session

--- app/src/main/java/org/aldev/aseli/views/LoginView.kt
+++ app/src/main/java/org/aldev/aseli/views/LoginView.kt
@@ -1,5 +1,6 @@
 package org.aldev.aseli.views
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,8 @@ class LoginView : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkForSession()
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -28,10 +31,11 @@ class LoginView : AppCompatActivity() {
             binding.btnLogin.isClickable = false
             viewModel.login()
         }
+        binding.btnToRegister.setOnClickListener { moveToRegister() }
     }
     private fun setOnLoginResult(){
         viewModel.loginFailed.observe(this) {
-            if(!it) return@observe
+            if(!it) return@observe setUserSession()
 
             binding.btnLogin.isClickable = true
             binding.loginAlert.visibility = View.VISIBLE
@@ -43,5 +47,24 @@ class LoginView : AppCompatActivity() {
             }
             binding.loginAlert.text = getString(failedId)
         }
+    }
+    private fun moveToRegister(){
+        startActivity(Intent(this, RegisterView::class.java))
+        finish()
+    }
+    private fun setUserSession(){
+        val pref = getSharedPreferences("user_data", MODE_PRIVATE).edit()
+        pref.putString("auth_key", viewModel.authKey)
+        pref.apply()
+
+        moveToHome()
+    }
+    private fun checkForSession(){
+        val pref = getSharedPreferences("user_data", MODE_PRIVATE)
+        if (pref.contains("auth_key")) moveToHome()
+    }
+    private fun moveToHome(){
+        startActivity(Intent(this, HomeView::class.java))
+        finish()
     }
 }
