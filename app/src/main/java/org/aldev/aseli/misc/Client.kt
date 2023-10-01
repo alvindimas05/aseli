672f3ac89136bea3c48a46a6f60339ded19f0e4a
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-10-01 21:25:44 +0700

    fix: auth required

--- app/src/main/java/org/aldev/aseli/misc/Client.kt
+++ app/src/main/java/org/aldev/aseli/misc/Client.kt
@@ -1,5 +1,6 @@
 package org.aldev.aseli.misc
 
+import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 
 class Client {
@@ -8,10 +9,13 @@ class Client {
 //        private const val url = "http://192.168.100.192:8080/query"
         private const val url = "$baseUrl/query"
         const val imagesUrl = "$baseUrl/images"
-        fun setClient(): ApolloClient {
-            return ApolloClient.Builder()
-                .serverUrl(url)
-                .build()
+        fun setClient(authKey: String? = null): ApolloClient {
+            var client = ApolloClient.Builder().serverUrl(url)
+            if(authKey != null) {
+                Log.d("Auth Key", authKey)
+                client = client.addHttpHeader("Auth-Key", authKey)
+            }
+            return client.build()
         }
     }
 }
