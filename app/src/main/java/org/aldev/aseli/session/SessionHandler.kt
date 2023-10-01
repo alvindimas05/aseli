b1ddc4d58661f2e940a0d2556af316c3d85aa0c6
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-10-01 21:26:43 +0700

    misc

--- app/src/main/java/org/aldev/aseli/session/SessionHandler.kt
+++ app/src/main/java/org/aldev/aseli/session/SessionHandler.kt
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 class SessionHandler (avt: Activity) {
     private val pref = avt.getSharedPreferences("user_data", AppCompatActivity.MODE_PRIVATE)
     fun checkSession(): Boolean {
-        return pref.getString("auth_key", "")!!.isNotEmpty()
+        return pref.contains("auth_key")
+    }
+    fun getSessionKey(): String {
+        return pref.getString("auth_key", "")!!
     }
     fun setUserSession(authKey: String){
         val edit = pref.edit()
