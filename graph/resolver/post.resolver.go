156353f490de491b8aeadbda84b7bd9bd114e504
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-10-13 09:47:33 +0700

    feat: post user profile image

--- graph/resolver/post.resolver.go
+++ graph/resolver/post.resolver.go
@@ -102,6 +102,7 @@ func (r *queryResolver) Posts(ctx context.Context, filter *model.PostsFilter) ([
 	}
 
 	fields = helper.ReplaceArrayValue(fields, "comments_total", "array::len(comments) AS comments_total")
+	fields = helper.ReplaceArrayValue(fields, "user_profile_image", "user.profile_image AS user_profile_image")
 
 	cfields := strings.Join(fields, ",")
 
@@ -122,7 +123,7 @@ func (r *queryResolver) Posts(ctx context.Context, filter *model.PostsFilter) ([
 			cndCmd += fmt.Sprintf("user.username='%s'", helper.SafelyConvertString(*filter.Username))
 		}
 	}
-	
+
 	query, err := db.Query(fmt.Sprintf("SELECT %s%s OMIT user FROM post %s", sfields[0], cmtCmd, cndCmd), nil)
 	if err != nil {
 		panic(err)
