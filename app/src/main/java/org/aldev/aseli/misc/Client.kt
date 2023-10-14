745a9f0f97a4d6c8d752f34b5b46018b9378c7af
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-10-14 15:47:17 +0700

    misc

--- app/src/main/java/org/aldev/aseli/misc/Client.kt
+++ app/src/main/java/org/aldev/aseli/misc/Client.kt
@@ -4,7 +4,7 @@ import com.apollographql.apollo3.ApolloClient
 
 class Client {
     companion object {
-        private const val baseUrl = "http://192.168.100.229:8080"
+        private const val baseUrl = "http://192.168.1.8:8080"
         private const val url = "$baseUrl/query"
         const val imagesUrl = "$baseUrl/images"
         fun setClient(authKey: String? = null): ApolloClient {
